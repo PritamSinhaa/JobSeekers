8dad2a4bd30b0e5b36f86eb240b7e6c920e92ace
PritamSinhaa <pritamsinha2323548@gmail.com>
2026-02-01 16:23:59 +0530

    Company Model created in mongodb

--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -1,1 +1,30 @@
 import mongoose from "mongoose";
+
+const companySchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    website: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    logo: {
+      type: String, // url to company logo
+    },
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Company = mongoose.model("Company", companySchema);
